8afb2b712efebfb1bbc6597ba0af62dadaff520c
CQY-ASUS-Ubuntu <pku_cqy@126.com>
2015-06-10 00:23:44 +0800

    save

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 import os
 import random
+from leancloud import File as LeanFile
 
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, request
 from flask import render_template, send_file, make_response, redirect
 
 from views.todos import todos_view
@@ -28,7 +29,15 @@ def another():
 	while os.path.isdir(chosen):
 		fn = random.choice(os.listdir(img_dir))
 		chosen = img_dir + fn
-	return chosen, 'baoman'
+	return chosen + '?type=baoman'
+
+@app.route('/show', methods=['POST'])
+def show():
+	print request.files
+	photo = request.files['photo']
+
+	photo.save(img_dir + 'photo.jpg')
+	return render_template("show.html")
 
 @app.route('/time')
 def time():
