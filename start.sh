b3b0edd3f4e4d39a23f46110de6d6f66d42927ff
CQY-ASUS-Ubuntu <pku_cqy@126.com>
2015-06-11 10:25:12 +0800

    run

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@ export LC_APP_ID="wy1vhkf58knzywjpmny6r1pqbywmy3zxqo1qmj35mmaizd0z"
 export LC_APP_KEY="10hyto051fgrtxib3uo5yie10s4da1jx500qjyk3qek24d0p"
 export LC_APP_MASTER_KEY="pye4tgaw8edmxlw7sct48xnb4r9h5lowdcufqokyug5cvy2q"
 export LC_APP_PORT=3100
-
 python wsgi.py
