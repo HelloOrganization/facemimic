a413acc83877e13d93bd1d592671dc0181851f4b
CQY-ASUS-Ubuntu <pku_cqy@126.com>
2015-06-11 14:23:18 +0800

    run app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import os
 import random
 import uuid
-from leancloud import File as LeanFile
+import leancloud
 
 from datetime import datetime
 
@@ -42,9 +42,9 @@ def result():
 	print photo_uuid
 	local_file_name = img_upload_dir + photo_uuid + ".jpg"
 	photo.save(local_file_name)
-	#photoFile = LeanFile('fileFromBuffer', buffer(photo))
+	#photoFile = leancloud.File('fileFromBuffer', buffer(photo))
 	local_file = open(local_file_name)
-	photo_file = LeanFile(photo_uuid, local_file)
+	photo_file = leancloud.File(photo_uuid, local_file)
 	local_file.close()
 	photo_file.save()
 	resp = make_response(render_template("result.html"))
@@ -64,3 +64,11 @@ def calc():
 @app.route('/time')
 def time():
     return str(datetime.now())
+
+if __name__ == '__main__':
+	APP_ID="wy1vhkf58knzywjpmny6r1pqbywmy3zxqo1qmj35mmaizd0z"
+	APP_KEY="10hyto051fgrtxib3uo5yie10s4da1jx500qjyk3qek24d0p"
+	MASTER_KEY="pye4tgaw8edmxlw7sct48xnb4r9h5lowdcufqokyug5cvy2q"
+	leancloud.init(APP_ID, master_key=MASTER_KEY)
+	port = int(os.environ.get("PORT", 6100))
+	app.run(host='0.0.0.0', port=port)
